fa87df851bea18c602fa8210a495dfd08c9e0137
gagome <danielbarragan@colegioguanenta.edu.co>
2023-07-18 06:34:33 -0500

    Comentarios en php

---
+++ 1_comentarios.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!--formas de incluir codigo php-->
+    <h1> COMENTARIOS EN PHP</h1>
+    <?php  
+        //comentario de una linea 
+        /*Esto es un comentario de varias lineas */
+    ?>
+    <!--
+    <%
+        codigo php
+    %>
+
+    <script languaje="php">
+    </script>--->
+</body>
+</html>
